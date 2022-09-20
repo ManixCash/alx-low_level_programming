edd07950804e12227cf7fd4b25f2d54bad8d60a8
ManixCash <iwattemma218@gmail.com>
2022-09-19 20:45:19 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 #define MAIN_H
 
 
-void _putchar (char c);
+int _putchar(char c);
 
 void reset_to_98(int *n);
 
@@ -24,6 +24,8 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
 
 
 
