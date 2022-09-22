7a71f72a501728e7442c02ede65360dee9e7666f
ManixCash <iwattemma218@gmail.com>
2022-09-21 22:15:39 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,3 @@
-#ifndef _main_h_
-
-#define _main_h_
-
-
-
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
@@ -16,13 +10,12 @@ int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
-char *string_toupper(char *);
+char *string_toupper(char *s);
 
-char *cap_string(char *);
+char *cap_string(char *s);
 
-char *leet(char *);
+char *leet(char *s);
 
 char *rot13(char *);
 
-
-#endif
+void print_number(int n);
