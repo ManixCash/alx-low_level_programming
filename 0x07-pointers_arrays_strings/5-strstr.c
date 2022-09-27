0508de37b16798a0521f737af20608b76fb1019d
ManixCash <iwattemma218@gmail.com>
2022-09-26 18:24:03 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,13 +8,9 @@
  * Return:  a pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
  */
-
 char *_strstr(char *haystack, char *needle)
-
 {
-
 char *startn = needle, *starth = haystack;
-
 while (*haystack)
 {
 starth = haystack;
