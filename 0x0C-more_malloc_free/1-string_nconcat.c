b33d08e9ad69ed76dff3c3c156a42d7f51e95729
ManixCash <iwattemma218@gmail.com>
2022-10-06 12:21:00 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,13 +8,9 @@
  * @n: an input integer of number of string to concatenate
  * Return: Apointer to concatened strings or NULL if it str is NULL
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-
 {
-
 char *new_str;
-
 unsigned int i = 0, lens1 = 0, lens2 = 0;
 
 if (s1 == NULL)
