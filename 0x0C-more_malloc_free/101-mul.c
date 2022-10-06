9a8e3eb6aa88fa737bd1f7858960e8df5d034b9b
ManixCash <iwattemma218@gmail.com>
2022-10-06 16:57:29 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,11 +9,8 @@
  */
 
 int main(int argc, char **argv)
-
 {
-
 int num1, num2, result;
-
 if (argc != 3)
 {
 printf("Error\n");
@@ -23,7 +20,7 @@ else
 {
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
+result = num1 *num2;
 printf("%d\n", result);
 return (0);
 }
