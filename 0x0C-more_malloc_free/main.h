8cfdf91a3ba392fe15ca9de936ec3c4f294a950e
ManixCash <iwattemma218@gmail.com>
2022-10-06 16:22:53 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,10 @@
-int _putchar(char c);
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+void _puts(char *str);
 
 void *malloc_checked(unsigned int b);
 
@@ -9,3 +15,15 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+int _putchar(char c);
+
+int _atoi(const char *s);
+
+void print_int(unsigned long int n);
+
+
+
+
+
+#endif
