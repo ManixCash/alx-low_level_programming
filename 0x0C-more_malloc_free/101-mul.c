4d45c5a830c7d26ae27db0ed8f140996a761884b
ManixCash <iwattemma218@gmail.com>
2022-10-06 16:32:05 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -22,7 +22,7 @@ for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
 {
-f (argv[i][j] > 57 || argv[i][j] < 48)
+f(argv[i][j] > 57 || argv[i][j] < 48)
 printf("Error\n");
 exit(98); }
 }
